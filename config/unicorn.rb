220f751802f21a18a3d55980da45c19eaa0df40c
Hau Nguyen <hau.nguyentrinh@asnet.com.vn>
2016-09-29 14:25:07 +0700

    update deployment code

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,14 +3,25 @@
 
 # Go with at least 1 per CPU core, a higher amount will usually help for fast
 # responses such as reading from a cache.
+app_dir = File.expand_path("../..", __FILE__)
+working_directory app_dir
+
+pid "#{app_dir}/tmp/unicorn.pid"
+
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
+
+listen "/tmp/unicorn.sock", :backlog => 64
+timeout 30
+
 worker_processes ENV['WORKER_PROCESSES'].to_i
 
 # Listen on a tcp port or unix socket.
-listen ENV['LISTEN_ON']
+# listen ENV['LISTEN_ON']
 
 # Use a shorter timeout instead of the 60s default. If you are handling large
 # uploads you may want to increase this.
-timeout 30
+# timeout 30
 
 # Combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings:
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
