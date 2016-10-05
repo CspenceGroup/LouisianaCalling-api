75a30521f66d3826de6c74e0cc0e0f8c259355e6
Oanh Do <oanh.do@asnet.com.vn>
2016-10-05 10:25:24 +0700

    Create  new contact info

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,8 +1,35 @@
 class ContactController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => :create
 
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      # Send email to Support team
+
+      flash[:notice] = "Your message have successfully sent out."
+      redirect_to contact_path
+    else
+      # Handling error messages
+      errors = @contact.errors.messages
+      msg_errors = []
+
+      errors.each do |key, values|
+        msg_errors << "#{key.capitalize} #{values[0]}"
+      end
+
+      flash[:errors] = msg_errors.flatten
+      redirect_to contact_path
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message, :subject)
   end
 end
