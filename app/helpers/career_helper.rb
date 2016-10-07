6be05fcf782824af236819f5e3b2ace7003b834f
Oanh Do <oanh.do@asnet.com.vn>
2016-10-07 10:39:53 +0700

    Refactor code

--- app/helpers/career_helper.rb
+++ app/helpers/career_helper.rb
@@ -63,23 +63,6 @@ module CareerHelper
     regions_query.join('')
   end
 
-  def industries_query_str(industry_ids)
-    industry_ids = industry_ids.split(',')
-    list_of_clusters = convert_industries_to_hash
-    industries_q = ['(']
-
-    industry_ids.each_with_index do |id, index|
-      if index.zero?
-        industries_q.push "industries like '%#{list_of_clusters[id.to_i]}%'"
-      else
-        industries_q.push " OR industries like '%#{list_of_clusters[id.to_i]}%'"
-      end
-    end
-    industries_q.push ')'
-
-    industries_q.join('')
-  end
-
   def skills_query_str(skill_ids)
     skill_ids = skill_ids.split(',')
     list_of_skills = convert_skills_to_hash
