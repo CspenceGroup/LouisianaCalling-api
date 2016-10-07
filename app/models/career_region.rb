b774704f6c7796d7044952ba0318d68929dd0cc0
Hau Nguyen <hau.nguyentrinh@asnet.com.vn>
2016-10-07 13:16:53 +0700

    refractor admin controller

--- app/models/career_region.rb
+++ app/models/career_region.rb
@@ -12,4 +12,26 @@ class CareerRegion < ApplicationRecord
   def should_generate_new_friendly_id?
     slug.blank? || title_changed?
   end
+
+  def self.import_from_csv(csv)
+    CareerRegion.transaction do
+      CareerRegion.delete_all
+
+      csv.each do |row|
+        careerRegion = CareerRegion.new
+        careerRegion[:title] = row[0].strip
+        # careerRegion[:slug] = row[0].parameterize
+        careerRegion[:region] = row[1].strip
+        careerRegion[:salary_min] = row[2].strip
+        careerRegion[:salary_max] = row[3].strip
+        careerRegion[:education] = row[4].strip
+
+        if row[5]
+          raise "Wrong file"
+        end
+
+        careerRegion.save!
+      end
+    end
+  end
 end
--- app/models/education.rb
+++ app/models/education.rb
@@ -1,3 +1,20 @@
 class Education < ApplicationRecord
   validates :name, presence: true
+
+  def self.import_from_csv(csv)
+    Education.transaction do
+      Education.delete_all
+
+      csv.each do |row|
+        education = Education.new
+        education[:name] = row[0].strip
+
+        if row[1]
+          raise "Wrong file"
+        end
+
+        education.save!
+      end
+    end
+  end
 end
