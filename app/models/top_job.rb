86992ddc4aca0abcb479c45cea5d283b8836cd6c
Oanh Do <oanh.do@asnet.com.vn>
2016-10-12 10:07:51 +0700

    Adding relationship between top_jobs model and region, career models

--- app/models/top_job.rb
+++ app/models/top_job.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: top_jobs
+#
+#  id             :integer          not null, primary key
+#  career_id     :integer
+#  region_id     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+#
+
 class TopJob < ActiveRecord::Base
   belongs_to :region
   belongs_to :career
@@ -17,7 +29,7 @@ class TopJob < ActiveRecord::Base
         career = Career.filter_by_title(row[1].strip).first
         top_jobs[:career_id] = career.id if career.present?
 
-        raise 'Wrong file' if row[2]
+        raise 'Wrong file' if row[2].present?
 
         top_jobs.save!
       end
