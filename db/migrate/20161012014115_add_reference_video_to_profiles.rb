6c7d352b3cbf545f6c39a9b0bfdb98062df13d23
Oanh Do <oanh.do@asnet.com.vn>
2016-10-12 10:07:51 +0700

    Adding relationship between top_jobs model and region, career models

--- db/migrate/20161012014115_add_reference_video_to_profiles.rb
+++ db/migrate/20161012014115_add_reference_video_to_profiles.rb
@@ -1,5 +1,5 @@
 class AddReferenceVideoToProfiles < ActiveRecord::Migration[5.0]
   def change
-    add_reference :profiles, :video, index: true, foreign_key: true
+    add_reference :profiles, :video, index: true
   end
 end
