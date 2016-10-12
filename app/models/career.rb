76812e6533cf87423e096cf2545cdf8616eddaf0
Oanh Do <oanh.do@asnet.com.vn>
2016-10-12 13:34:24 +0700

    Create career_interests model

--- app/models/career.rb
+++ app/models/career.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: careers
+#
+#  id             :integer          not null, primary key
+#  profile_id     :integer
+#  title          :string
+#  url            :string
+#  description    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+#
+
 class Career < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug_by_title, use: :slugged
@@ -26,31 +40,21 @@ class Career < ActiveRecord::Base
   has_many :related_by_interests, through: :career_interest_relationships,
                                   source: :career_related
 
-  belongs_to :education
-
-  # serialize :regions_high_demand, Array
-  # serialize :interests, Array
-  # serialize :skills, Array
-  # serialize :industries, Array
-  # serialize :related_career_by_skill, Array
-  # serialize :related_career_by_interest, Array
-  # serialize :region, Array
+  has_many :educations
 
   validates :title, presence: true
-  validates :education, presence: true
+  # validates :education, presence: true
   validates :about_job, presence: true
   validates :what_will_do, presence: true
-  # validates :profile_name, presence: true
-  validates :photo_large, presence: true
-  validates :photo_medium, presence: true
-  # validates :photo_small, presence: true
-  validates :industries, presence: true
-  validates :interests, presence: true
-  validates :skills, presence: true
+  # validates :photo_large, presence: true
+  # validates :photo_medium, presence: true
+  # validates :industries, presence: true
+  # validates :interests, presence: true
+  # validates :skills, presence: true
   validates :salary_min, presence: true
   validates :salary_max, presence: true
   validates :demand, presence: true
-  validates :regions_high_demand, presence: true
+  # validates :regions_high_demand, presence: true
 
   scope :filter_by_title, lambda { |titles|
     where('title IN (?)', titles)
@@ -64,6 +68,54 @@ class Career < ActiveRecord::Base
     where('salary_max <= ? AND salary_min >= ?', salary_max, salary_min)
   }
 
+  def self.import_from_csv(csv)
+    Career.transaction do
+      Career.delete_all
+
+      csv.each do |row|
+        career = Career.new
+        career[:title] = row[0].strip
+        # career[:industries] = []
+
+        # (1..4).each do |i|
+        #   career[:industries] << row[i].strip if row[i].present?
+        # end
+
+        # career[:skills] = row[5].split(',').map(&:strip)
+        # career[:interests] = row[6].split(',').map(&:strip)
+        career[:salary_min] = row[7].strip
+        career[:salary_max] = row[8].strip
+        # career[:education] = row[9].strip
+        career[:about_job] = row[10].strip
+        career[:what_will_do] = row[11].strip
+        # career[:related_career_by_skill] = row[12].split(';').map(&:strip) if row[12]
+        # career[:related_career_by_interest] = row[13].split(';').map(&:strip) if row[13]
+        career[:demand] = row[14].strip
+        career[:photo_large] = row[15].strip if row[15]
+        career[:photo_medium] = row[16].strip if row[16]
+        # career[:regions_high_demand] = row[17].split(',').map(&:strip)
+        # career[:profile_name] = row[18].strip if row[18]
+
+        raise 'Wrong file' if row[19]
+
+        career.save!
+        interests = row[6].split(',').map(&:strip)
+        create_career_interest(interests, career)
+      end
+    end
+  end
+
+  def self.create_career_interest(interests, career)
+    interests.each do |interest_name|
+      interest = Interest.find_by_name(interest_name)
+
+      CareerInterest.create(
+        career_id: career.id,
+        interest_id: interest.present? ? interest.id : nil
+      )
+    end
+  end
+
   private
 
   # Defaults a slug with title
@@ -73,41 +125,5 @@ class Career < ActiveRecord::Base
 
   def should_generate_new_friendly_id?
     slug.blank? || title_changed?
-  end
-
-  def self.import_from_csv(csv)
-    Career.transaction do
-      Career.delete_all
-
-      csv.each do |row|
-        career = Career.new
-        career[:title] = row[0].strip
-        # career[:slug] = row[0].parameterize
-        career[:industries] = []
-
-        (1..4).each do |i|
-          career[:industries] << row[i].strip if row[i].present?
-        end
-
-        career[:skills] = row[5].split(',').map(&:strip)
-        career[:interests] = row[6].split(',').map(&:strip)
-        career[:salary_min] = row[7].strip
-        career[:salary_max] = row[8].strip
-        career[:education] = row[9].strip
-        career[:about_job] = row[10].strip
-        career[:what_will_do] = row[11].strip
-        career[:related_career_by_skill] = row[12].split(';').map(&:strip) if row[12]
-        career[:related_career_by_interest] = row[13].split(';').map(&:strip) if row[13]
-        career[:demand] = row[14].strip
-        career[:photo_large] = row[15].strip if row[15]
-        career[:photo_medium] = row[16].strip if row[16]
-        career[:regions_high_demand] = row[17].split(',').map(&:strip)
-        career[:profile_name] = row[18].strip if row[18]
-
-        raise 'Wrong file' if row[19]
-
-        career.save!
-      end
-    end
   end
 end
