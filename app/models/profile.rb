b5248bd06628e95b4a3a46ddcbe5385ad3f4ef25
Oanh Do <oanh.do@asnet.com.vn>
2016-10-14 13:56:32 +0700

    Adding relationship for profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,8 +10,6 @@ class Profile < ActiveRecord::Base
 
   has_many :videos
 
-  has_many :videos
-
   has_many :profile_interests, dependent: :destroy
   has_many :interests, through: :profile_interests, source: :interest
 
