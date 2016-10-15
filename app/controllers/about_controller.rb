492ac90bd0ce9cde975363d4f12e8321f354e3fa
Oanh Do <oanh.do@asnet.com.vn>
2016-10-15 21:11:15 +0700

    Handle view/send contact us

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,36 @@
 class AboutController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
   def index
+    @contact = Contact.new
+    @tab = params[:tab]
+    @tag |= 'who-we-are'
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      flash[:notice] = 'Your message has successfully sent out.'
+    else
+      # Handling error messages
+      errors = @contact.errors.messages
+      msg_errors = []
+
+      errors.each do |key, values|
+        msg_errors << "#{key.capitalize} #{values[0]}"
+      end
+
+      flash[:errors] = msg_errors.flatten
+    end
+
+    @tab = 'contact-us'
+    render :index
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message, :subject)
   end
 end
