7a64ede8841d07dee0074c613f280a2f75bb2f51
Oanh Do <oanh.do@asnet.com.vn>
2016-10-17 08:45:58 +0700

    Show about us page from footer URL

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -3,21 +3,49 @@ $(document).on('turbolinks:load', function(){
   /****************************************
    *             About                  *
    ****************************************/
+  var changeURL = function(page, url) {
+    if (typeof(history.pushState) != 'undefined') {
+      var obj = { page: page, url: url };
+      history.pushState(obj, page, url);
+    } else {
+      console.log("Browser does not support HTML5.");
+    }
+  };
 
-  $('.faq-contact-btn').on('click', function() {
-    // Hidden alert
+  $( document ).ready(function() {
+    if (window.location.pathname.indexOf('abouts') != -1) {
+      changeURL('About us', 'abouts')
+    }
+  });
+
+  $('.faq-contact-btn').on('click', function(e) {
+    e.preventDefault();
+    // Hidden alert info
     $('.about-contact-container').find('.alert').hide();
-
     $('#about-tabs a[href="#contactUs"]').tab('show');
   });
 
   $('#faq-footer').on('click', function(e) {
     e.preventDefault();
-    $('#about-tabs a[href="#faq"]').tab('show');
+
+    // Currently, About us page
+    if (window.location.pathname.indexOf('abouts') != -1) {
+      $('#about-tabs a[href="#faq"]').tab('show');
+    } else {
+      // Goto About us page with FAQ tab
+      window.location.href = '/abouts'
+    }
   });
 
   $('#contact-footer').on('click', function(e) {
     e.preventDefault();
-    $('#about-tabs a[href="#contactUs"]').tab('show');
+
+    // Currently, About us page
+    if (window.location.pathname.indexOf('abouts') != -1) {
+      $('#about-tabs a[href="#contactUs"]').tab('show');
+    } else {
+      // Goto About us page with contact-us tab
+      window.location.href = '/abouts'
+    }
   });
 });
