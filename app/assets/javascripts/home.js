834e0f3dfb630dd2a3c59df5741df906b2b3cf9f
Oanh Do <oanh.do@asnet.com.vn>
2016-10-17 15:46:30 +0700

    Get list educations in career page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -157,7 +157,7 @@ $(document).on('turbolinks:load', function(){
     if (activeRegionData) {
       var educations = activeRegionData.educations.map(function(edu) { return edu.name; })
       $('#career-salary').html('<i class="icon i-options i-salary"></i><span class="career-options__text">$' + numberWithCommas(activeRegionData.salary_min) + ' - $' + numberWithCommas(activeRegionData.salary_max) +'</span>');
-      $('#career-certificate').html('<i class="icon i-options i-certificate"></i><span class="career-options__text" title="'+ educations.toString() + '">' + educations.toString() +'</span>');
+      $('#career-certificate').html('<i class="icon i-options i-certificate"></i><span class="career-options__text" title="'+ educations.join(', ') + '">' + educations.join(', ') +'</span>');
     }
   }
 
