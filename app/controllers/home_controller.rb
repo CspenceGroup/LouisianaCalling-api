2c766a585589fc38f066a3738e7cf839be3bc5f2
Oanh Do <oanh.do@asnet.com.vn>
2016-10-24 13:30:12 +0700

    Remove data top_job

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,5 @@ class HomeController < ApplicationController
     @videos = Video.all
     @regions = Region.all
     @careers_slider = Career.first(5)
-
-    TopJob.all.valid.group_by(&:region).each do |region, top_jobs|
-      @jobs[region.name] = top_jobs.map do |top_job|
-        {
-          job: top_job.career.title,
-          link: career_detail_path(top_job.career)
-        }
-      end
-    end
   end
 end
