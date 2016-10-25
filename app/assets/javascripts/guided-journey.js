e3a94e015010682008b29dfa5d1d3df6382e68ba
Oanh Do <oanh.do@asnet.com.vn>
2016-10-25 16:00:46 +0700

    Implement get results for guided journey

--- app/assets/javascripts/guided-journey.js
+++ app/assets/javascripts/guided-journey.js
@@ -30,7 +30,7 @@ $(document).on('turbolinks:load', function(){
    * This function handle the action when user click on region map
    * @return void
    */
-  
+
   $('.guided-journey-map path').click(function (e) {
 
     var value = $(this).attr('id'),
@@ -51,7 +51,7 @@ $(document).on('turbolinks:load', function(){
         $('.sidebar-steps-result__content-region p[title="' + value + '"]').remove();
       }
     } else {
-      
+
       if($(this).hasClass('lafayette')) {
 
         $('.lafayette').addClass('active');
@@ -62,8 +62,6 @@ $(document).on('turbolinks:load', function(){
         regionContent.append('<p title="' + value + '">'+ value + '</p>');
       }
     }
-
-    
   });
 
   /**
@@ -107,4 +105,22 @@ $(document).on('turbolinks:load', function(){
     }
   });
 
+  function getCareerResults(data) {
+    $.ajax({
+      url : '/guided_journey/search',
+      type : "get",
+      dateType:"text",
+      traditional: true,
+      data : data,
+      success: function(response) {
+        // Update limit/offset
+        $('#careerLimit').val(response.limit);
+        $('#careerOffset').val(response.offset);
+
+      },
+      error: function() {
+
+      }
+    });
+  }
 });
--- app/controllers/guided_journey_controller.rb
+++ app/controllers/guided_journey_controller.rb
@@ -1,7 +1,51 @@
 class GuidedJourneyController < ApplicationController
+  include CareerHelper
   before_filter :datas_for_binding, only: [:index]
 
   def index
+  end
+
+  def search
+    @limit = params[:limit] || 9
+    @offset = params[:offset] || 0
+
+    @limit = @limit.to_i
+    @offset = @offset.to_i
+
+    careers = Career.all
+
+    ## filter by regions
+    if params[:regions].present?
+      careers = careers.filter_by_region(params[:regions].split(','))
+    end
+
+    # ## filter by skill
+    if params[:skills].present?
+      careers = careers.filter_by_skill(params[:skills].split(','))
+    end
+
+    ## filter by education
+    if params[:educations].present?
+      careers = careers.filter_by_education(params[:educations].split(','))
+    end
+
+    ## filter by interests
+    if params[:interests].present?
+      careers = careers.filter_by_interest(params[:interests].split(','))
+    end
+
+    next_offset = @limit + @offset
+    is_see_more = careers.count > next_offset ? true : false
+    careers = careers.offset(@offset).limit(@limit)
+
+    render json: {
+      careers: render_to_string(
+        'guided_journey/partial/guided-career-result', layout: false, locals: { careers: careers }
+      ),
+      is_see_more: is_see_more,
+      offset: next_offset,
+      limit: @limit
+    }
   end
 
   private
