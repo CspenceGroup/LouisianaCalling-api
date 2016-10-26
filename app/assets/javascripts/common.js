6655030401658546e8d8e1647982cd3268f6b4df
Oanh Do <oanh.do@asnet.com.vn>
2016-10-26 09:18:53 +0700

    Handle click into map region

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,8 +1,23 @@
-  /**
-   * Replace space in string with %20
-   * @param  {string} value
-   * @return {string}
-   */
-  function convertToUrl (value) {
-    return value.split(' ').join('%20');
+/**
+ * COMMON FUNCTIONS
+ */
+/**
+ * Replace space in string with %20
+ * @param  {string} value
+ * @return {string}
+ */
+function convertToUrl (value) {
+  return value.split(' ').join('%20');
+}
+
+/**
+ * Convert ID get from map path into Region Name
+ * @param  {string} name ID of map's path
+ * @return {string} Region Name after convert
+ */
+function convertToRegionName (name) {
+  if (name) {
+    return name.split('-').join(' ');
   }
+
+}
