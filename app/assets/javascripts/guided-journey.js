800a253db384b6191c96ae0c9bd56c033c75ca40
Huy Bich Nguyen <huy.nguyenbich@asnet.com.vn>
2016-10-27 10:27:04 +0700

    Binded career name result data, fixed displaying jump start

--- app/assets/javascripts/guided-journey.js
+++ app/assets/javascripts/guided-journey.js
@@ -149,7 +149,7 @@ $(document).on('turbolinks:load', function(){
     checkResultStep(contentEducation);
 
     if (value.toLowerCase() === text) {
-      $('.guided-journey-results').toggleClass("jump-start-inactive");
+      $('.jump-start-guide').toggleClass("jump-start-inactive");
     }
 
     $('.sidebar-steps-result__content-education').show();
