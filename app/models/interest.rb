d21fb46e2c2a406b53eaeb0d35bbb640aeb013fd
Oanh Do <oanh.do@asnet.com.vn>
2016-10-31 14:39:39 +0700

    Update import interests

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -23,20 +23,42 @@ class Interest < ActiveRecord::Base
 
   validates_uniqueness_of :name
 
+  def self.update_interest(params)
+    interest = Interest.find_by_name(params[:name])
+
+    raise "Do not found with interest: '#{params[:name]}'." unless interest.present?
+
+    interest.update_attributes(params)
+  end
+
+  def self.find_interest(interest_name)
+    interest = Interest.find_by_name(interest_name)
+    unless interest.present?
+      raise "Do not found with interest: '#{interest_name}'.
+        Please make sure import Interest before."
+    end
+
+    interest
+  end
+
   def self.import_from_csv(csv)
     Interest.transaction do
-      Interest.delete_all
+      # Interest.delete_all
 
       csv.each do |row|
-        name_str = row[0].strip
-        next if Interest.exists?(name: name_str)
+        params = {
+          name: row[0].strip,
+          url: row[1].strip,
+          url_selected: row[2].strip
+        }
 
-        interest = Interest.new
-        interest[:name] = name_str
-        interest[:url] = row[1].strip
-        interest[:url_selected] = row[2].strip
-
-        interest.save!
+        if Interest.exists?(name: params[:name])
+          # Update interest
+          Interest.update_interest(params)
+        else
+          # Create interest
+          Interest.create(params)
+        end
       end
     end
   end
