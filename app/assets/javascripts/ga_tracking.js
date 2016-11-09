4a734f0cc2cd27b9bc837f53cf6478854c249944
Oanh Do <oanh.do@asnet.com.vn>
2016-11-09 10:15:05 +0700

    Update Ga

---
+++ app/assets/javascripts/ga_tracking.js
@@ -0,0 +1,19 @@
+'use strict';
+$(document).on('turbolinks:load', function() {
+  var pageview = window.location.pathname,
+    title = document.title;
+
+  if (pageview == '/abouts') {
+    title = 'About Us - Louisiana Calling'
+  }
+
+  var page = {
+    page: pageview,
+    title: title
+  };
+
+  if (window.ga) {
+    ga('send', 'pageview', page);
+    console.log(title, pageview);
+  }
+});
